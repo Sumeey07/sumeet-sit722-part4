4937c360fc38479965fb434c779d027b8a555e36
Sumeet Kumar <s222615086@deakin.edu.au>
2024-09-23 05:05:33 +1000

    Mid Commit

---
+++ terraform/providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type    = string
+  default = "deakinuni"
+}
+
+variable "location" {
+  type    = string
+  default = "Australia Southeast"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "sit722part4"
+}
+
+variable "kubernetes_cluster_name" {
+  type    = string
+  default = "sit722part4"
+}
